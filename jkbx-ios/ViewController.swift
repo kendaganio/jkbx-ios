496bd70bb22202f96af142e24c0386d9ac5e3a8d
Ken-Lauren Daganio <akosiken@gmail.com>
2015-07-13 15:39:48 +0800

    Initial Commit

---
+++ jkbx-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jkbx-ios
+//
+//  Created by Ken-Lauren Daganio on 7/13/15.
+//  Copyright (c) 2015 Ken-Lauren Daganio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
